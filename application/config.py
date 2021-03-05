fb0e4f10dc8b5c4feb7f69f2f7108c5c18dfb955
mehdi <41826698+mahdinazari@users.noreply.github.com>
2021-03-05 19:43:54 +0330

    Implemented login member API (#4)

--- application/config.py
+++ application/config.py
@@ -1,16 +1,20 @@
 import os
+from datetime import timedelta
 
 
 class Config(object):
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_BINDS = False
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'SQLALCHEMY_DATABASE_URI',
         'postgres://postgres:postgres@localhost/solace'
     )
 
-    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    # JWT Secret key
+    JWT_SECRET_KEY = '@ASDASDOWIQ!@&EQHC<XNYWGYW#!@'
+    JWT_EXPIRES_DELTA = timedelta(days=10)
 
     APPLICATION_VIEWS = [
         'member',
--- views/member.py
+++ views/member.py
@@ -1,9 +1,15 @@
 from flask import jsonify, Blueprint, request
+from werkzeug.security import check_password_hash
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token, get_jwt_identity, \
+    create_refresh_token
+)
 
 from application.app import db
+from application.config import Config
 from models.member import Member, MemberSchema
 from application.exceptions import EmptyList, PasswordNotInForm, \
-    FullnameNotInForm, EmailNotInForm, DuplicateMemberFound
+    FullnameNotInForm, EmailNotInForm, DuplicateMemberFound, MemberNotFound
 
 
 blueprint = Blueprint('models/member', __name__, url_prefix='/api/v1/member')
@@ -58,4 +64,52 @@ def register():
         db.session.rollback()
 
     return jsonify(MemberSchema().dump(member)), 200
+
+
+@blueprint.route('/login', methods=['POST'])
+def login():
+    if not request.json:
+        raise EmptyList()
+
+    if 'email' not in request.json and 'email' not in request.form:
+        raise EmailNotInForm()
+
+    if 'password' not in request.json and 'password' not in request.form:
+        raise PasswordNotInForm()
+
+    email = request.json.get('email')
+    if not email:
+        email = request.form.get('email')
+
+    if not email:
+        raise EmailNotInForm()
+
+    password = request.json.get('password')
+    if not password:
+        password = request.form.get('password')
+
+    if not password:
+        raise PasswordNotInForm()
+
+    member = Member.query \
+        .filter(Member.email == email) \
+        .first()
+    if not member or member.is_deleted:
+        raise MemberNotFound()
+
+    expires = Config.JWT_EXPIRES_DELTA
+    if check_password_hash(member.hashed_password, password):
+        access_token = create_access_token(
+            identity=member.id,
+            additional_claims = {"email": member.email},
+            expires_delta=expires,
+        )
+        refresh_token = create_refresh_token(identity=member.id)
+
+        return jsonify(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        ), 200
+
+    raise MemberNotFound()
 
