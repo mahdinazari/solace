830a47411cd570ba921fd70fe34a7795cd9aa015
mehdinazari <mehdi.nazari.3727@gmail.com>
2021-03-05 13:58:23 +0330

    Done

--- application/app.py
+++ application/app.py
@@ -14,9 +14,9 @@ def create_app(config_filename):
     migrate.init_app(app, db)
     ma.init_app(app)
 
-#    with app.app_context():
-#        db.init_app(app)
-#
-#    app.app_context().push()
+    with app.app_context():
+        db.init_app(app)
+
+    app.app_context().push()
     return app
 
